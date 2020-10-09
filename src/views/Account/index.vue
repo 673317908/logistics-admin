0b30ec03bad82458c7695126f2c800a9862578e8
673317908Liyi <liy@sayingdata.com>
2020-10-09 17:56:30 +0800

    添加应收款管理页面

---
+++ src/views/Account/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    应收款管理
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="less">
+</style>
