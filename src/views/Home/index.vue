10494eb29a58a7fb4808e1ebb79c061e1d842718
673317908Liyi <liy@sayingdata.com>
2020-10-09 17:56:58 +0800

    添加首页

---
+++ src/views/Home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <!-- <Index /> -->
+    首页
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Index from "../../layout/index";
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
